b297dcc43b22bd304d32f96a59d224d2a451a10a
vicent <603413691@qq.com>
2021-06-15 23:31:22 +0800

    change

--- my-site/src/main.js
+++ my-site/src/main.js
@@ -1,24 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import "./styles/global.less"
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import App from './App.vue';
+import "./styles/global.less";
+import router from "./router";
 
-import Home from "@/views/Home";
-import Blog from "@/views/Blog";
-import About from "@/views/About";
-import Project from "@/views/Project";
-import Message from "@/views/Message";
-Vue.use(VueRouter);
-const router = new VueRouter({
-    mode: "history",
-    routes: [
-        { name: "Home", path: "/", component: Home },
-        { name: "Blog", path: "/blog", component: Blog },
-        { name: "About", path: "/about", component: About },
-        { name: "Project", path: "/project", component: Project },
-        { name: "Message", path: "/message", component: Message }
-    ]
-})
 
 new Vue({
     router,
---
+++ my-site/src/router/routes.js
@@ -0,0 +1,13 @@
+import Home from "@/views/Home";
+import Blog from "@/views/Blog";
+import About from "@/views/About";
+import Project from "@/views/Project";
+import Message from "@/views/Message";
+
+export default [
+    { name: "Home", path: "/", component: Home },
+    { name: "Blog", path: "/blog", component: Blog },
+    { name: "About", path: "/about", component: About },
+    { name: "Project", path: "/project", component: Project },
+    { name: "Message", path: "/message", component: Message }
+]
